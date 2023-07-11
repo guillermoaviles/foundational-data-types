eec94bfd27bf3b2ecd3dd37b272455bd7a3b6346
Guillermo Aviles <gjaviles7@gmail.com>
2023-07-11 11:39:23 -0400

    array list, add elements to array

--- src/com/ironhack/w1/d2/Main.java
+++ src/com/ironhack/w1/d2/Main.java
@@ -1,6 +1,8 @@
 package com.ironhack.w1.d2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -37,6 +39,49 @@ public class Main {
         for (int number : evenNumbers) {
             System.out.println(number);
         }
+
+        // WHILE LOOP
+
+        int counter = 0;
+        while (counter < evenNumbers.length) {
+            counter++;
+            System.out.println("Counter: " + counter);
+        }
+
+        // MATH CLASS
+
+        System.out.println(Math.sqrt(64));
+        System.out.println((int) Math.pow(2, 5));
+        System.out.println((int) Math.ceil(3.72));
+
+        // ARRAY LIST
+
+        List<String> countries = new ArrayList<>();
+        countries.add("Puerto Rico");
+        countries.add("Spain");
+        countries.add("France");
+        System.out.println(countries);
+        System.out.println(countries.get(0));
+        countries.set(2, "Italy");
+        System.out.println(countries);
+        System.out.println(countries.size());
+        countries.remove(2);
+        System.out.println(countries);
+
+        for (int i = 0; i < countries.size(); i++) {
+            System.out.println(countries.get(i));
+        }
+
+        List<Integer> integerList  = new ArrayList<>();
+
+
+        // ADD ELEMENTS TO ARRAY
+
+        int[] numbers = {0, 1, 3, 5, 6};
+        // numbers[5] = 5;  // This is not possible with an array
+
+        int[] newArray = new int[6];
+
     }
     public static String[] createSubjectsArray() {
         String[] subjects = new String[5];
